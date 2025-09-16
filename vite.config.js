3b3ab0ba78fc8d7dfb126b08aa3078ee8f94e383
SAM.AM <samir.amri@gmail.com>
2025-09-16 11:42:25 +0000

    fix: Update vite config with correct base path

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,15 @@ export default defineConfig({
   // It needs to start and end with a slash.
   base: "/codespaces-react/", 
   plugins: [react()],
+  // THE FIX: Add this 'build' section to change the output directory.
+  build: {
+    outDir: 'docs' // This tells Vite to create a 'docs' folder instead of 'dist'.
+  },
   test: {
     globals: true,
     environment: 'jsdom',
   },
 });
+
+    
+
