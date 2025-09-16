05617d0db730af05e6a4e46d370d91aa2056e2b0
SAM.AM <samir.amri@gmail.com>
2025-09-16 10:38:24 +0000

    update gitignore

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "FborderAutht": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This 'resolve' section is added for compatibility with AWS Amplify
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  // This 'define' section is also added for AWS Amplify
+  define: {
+    'global': {},
+  },
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
